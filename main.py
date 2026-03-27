35248d162fc09e4b6c527cd2428c5eeef259571d
rabwplen <thekovalcukdanylo@gmail.com>
2026-03-27 13:16:02 +0200

    change ui and media info logic for better performance i guess

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import asyncio
+import threading
 
 import tkinter as tk
 import customtkinter as ctk
@@ -8,73 +9,8 @@ import customtkinter as ctk
 from winrt.windows.media.control import GlobalSystemMediaTransportControlsSessionManager
 
 from PIL import Image, ImageDraw, ImageTk
-    
 
-def quit_app():
-    print("bye!")
-    app.quit()
-    
-async def get_media_info():
-    sessions = await GlobalSystemMediaTransportControlsSessionManager.request_async()
-    session = sessions.get_current_session()
-
-    if not session:
-        print("No active media session (get_media_info)")
-        return
-
-    info = await session.try_get_media_properties_async()
-    
-
-    return {
-        "title":    info.title or "-",
-        "artist":   info.artist or "-",
-        "album":    info.album_title or "-",
-        "album_artist": info.album_artist or "-"
-        # "thumbnail": info.thumbnail or "-"
-    }
-    
-async def get_main():
-    data = await get_media_info()
-
-    if not data:
-        print("No active media session (main)")
-        return
-
-    print(" - - - Current Media - - - ")
-    for key, value in data.items():
-        print(f"{key}: {value}")
-    print()
-
-class MediaInfoWindow(ctk.CTkToplevel):
-    def __init__(self, master):
-        super().__init__(master)
-        self.title("Media Info")
-        self.geometry("400x200")
-        
-        self.update_button = ctk.CTkButton(self, text="Get Media Info", command=self.on_get_media_info)
-        self.update_button.pack(pady=20)
-        
-        self.title_label = ctk.CTkLabel(self, text="Title: -")
-        self.title_label.pack()
-        self.artist_label = ctk.CTkLabel(self, text="Artist: -")
-        self.artist_label.pack()
-        self.album_label = ctk.CTkLabel(self, text="Album: -")
-        self.album_label.pack()
-        
-    def on_get_media_info(self):
-        asyncio.run(get_main())
-        data = asyncio.run(get_media_info())
-        
-        if data:
-            # Если данные есть, обновляем текст
-            self.title_label.configure(text=f"Title: {data['title']}")
-            self.artist_label.configure(text=f"Artist: {data['artist']}")
-            self.album_label.configure(text=f"Album: {data['album']}")
-        else:
-            # Если плеера нет, выводим заглушку, чтобы не было ошибки
-            self.title_label.configure(text="Title: No Media Active")
-            self.artist_label.configure(text="Artist: -")
-            self.album_label.configure(text="Album: -")
+from media_info import get_media_info, print_media_info, load_media_info
 
 
 # UIs basic colors for Overlay
@@ -83,7 +19,14 @@ COLOR_SECOND_BACKGROUND = "#080808"
 COLOR_MAIN_TEXT = "#FFFFFF"
 COLOR_SECONDARY_TEXT = "#BEBEBE"
 COLOR_MUTED_TEXT = "#6E6E6E"
-COLOR_CONTROL_PANEL = "#000000"
+
+# Standard UIs Positions
+POS_BACKGROUND = {'relx': 0, 'rely': 0, 'x': 20, 'y': 0, "anchor": "nw"}
+
+POS_CLOSE_BUTTON = {'relx': 0, 'rely': 0, 'x': 0, 'y': 0, "anchor": "nw"}
+POS_SETTINGS_BUTTON = {'relx': 0, 'rely': 0, 'x': 2, 'y': 28, "anchor": "nw"}
+POS_DRAG_HANDLE = {'relx': 0, 'rely': 1, 'x': 0, 'y': 0, "anchor": "sw"}
+POS_RESIZE_GRIP = {'relx': 1, 'rely': 1, 'x': 0, 'y': 0, "anchor": "se"}
 
 class Overlay(ctk.CTk):
     def __init__(self):
@@ -94,36 +37,49 @@ class Overlay(ctk.CTk):
         self.overrideredirect(True)
         
         
-        self.backgound_frame = ctk.CTkFrame(self, fg_color=COLOR_BACKGROUND, bg_color="transparent", width=330-20, height=0)
-        self.backgound_frame.place(x=20, y=0, relheight=1)
+        self.main_background = ctk.CTkFrame(self, fg_color=COLOR_BACKGROUND, bg_color="transparent")
+        self.main_background.place(relx=0, rely=0, relwidth=1, relheight=1)
         
-        self.side_buttons = ctk.CTkFrame(self,fg_color=COLOR_BACKGROUND, bg_color="transparent", width=20, height=0)
-        self.side_buttons.place(x=0, y=0, relheight=1)
+        # track info frame
+        self.background = ctk.CTkFrame(self, fg_color=COLOR_BACKGROUND, bg_color=COLOR_BACKGROUND, height=self.winfo_height(), width=(self.winfo_width()-20))
+        self.background.place(**POS_BACKGROUND)
         
         
-        self.close_icon = ctk.CTkImage(light_image=Image.open("assets/close-icon.png"), size=(8, 8))
+        self.close_icon = ctk.CTkImage(light_image=Image.open("assets/close-icon.png"), size=(10, 10))
         
-        self.close_button = ctk.CTkButton(self.side_buttons, text="", image=self.close_icon,
-                                       width=20, height=20,
-                                       fg_color="#ff0000", hover_color="#ff4d4d",
+        self.close_button = ctk.CTkButton(self.main_background, text="", image=self.close_icon,
+                                       width=20, height=20, corner_radius=0,
+                                       fg_color=COLOR_BACKGROUND, hover_color="#1A1A1A",
                                        command=quit_app)
-        self.close_button.place(x=0, y=0, rely=0, anchor="nw")
+        self.close_button.place(**POS_CLOSE_BUTTON)
 
-        self.drag_handle = ctk.CTkFrame(self.side_buttons,
-                                        width=20, height=20,
-                                        fg_color="#ECECEC")
-        self.drag_handle.place(x=0, y=0, rely=1, anchor="sw")
+        self.drag_icon = ctk.CTkImage(light_image=Image.open('assets/drag-icon.png'), size=(10, 10))
+
+        self.drag_handle = ctk.CTkLabel(self.main_background, text="", image=self.drag_icon,
+                                        width=20, height=20, corner_radius=0,
+                                        fg_color=COLOR_BACKGROUND)
+        self.drag_handle.place(**POS_DRAG_HANDLE)
         self.drag_handle.bind("<Button-1>", self.start_move)
         self.drag_handle.bind("<B1-Motion>", self.do_move)
+
         
-        self.title_label = ctk.CTkLabel(self.backgound_frame, text="Title: -")
-        self.title_label.place(x=2, y=0)
+        self.title_label = ctk.CTkLabel(self.background, text="Title", font=ctk.CTkFont(size=18, weight="bold",family="Segoe UI"), height=20,
+                                        text_color=COLOR_MAIN_TEXT)
+        self.title_label.place(x=95, y=0, anchor="nw")
         
-        self.artist_label = ctk.CTkLabel(self.backgound_frame, text="Artist: -")
-        self.artist_label.place(x=2, y=25)
+        self.artist_label = ctk.CTkLabel(self.background, text="Artist", font=ctk.CTkFont(size=14, family="Segoe UI Semibold"),
+                                        text_color=COLOR_SECONDARY_TEXT)
+        self.artist_label.place(x=95, y=22, anchor="nw")
         
-        self.album_label = ctk.CTkLabel(self.backgound_frame, text="Album: -")
-        self.album_label.place(x=2, y=50)
+        self.track_duration = ctk.CTkLabel(self.background, text="0:00 / 0:00", font=ctk.CTkFont(size=12, family="Segoe UI"), text_color=COLOR_MUTED_TEXT)
+        self.track_duration.place(x=95, y=-2, rely=1, anchor="sw")
+        
+        self.track_cover = ctk.CTkLabel(self.background, text="", width=90, height=90)
+        self.track_cover.place(x=0, y=0, rely=0.5, anchor="w")
+        
+        
+        self.update_button = ctk.CTkButton(self, text="get media info", corner_radius=0, width=20, command=on_get_media_info)
+        self.update_button.place(x=0, y=0, relx=1, rely=1, anchor="se")
     
     
     # window dragging logic
@@ -140,7 +96,59 @@ class Overlay(ctk.CTk):
 
 
 
+def update_ui(data):
+    if not data:
+        app.title_label.configure(text="No Media Active")
+        app.artist_label.configure(text="Unknown artist")
+        app.track_duration.configure(text="0:00 / 0:00")
+        app.track_cover.configure(image=None, text="")
+        return
+
+    app.title_label.configure(text=data["title"])
+    app.artist_label.configure(text=data["artist"])
+    app.track_duration.configure(text=f"{data['position']} / {data['duration']}")
+    
+    if data["cover_image"]:
+        img = data["cover_image"].resize((90, 90))
+        ctk_img = ctk.CTkImage(light_image=img, dark_image=img, size=(90, 90))
+        app.track_cover.configure(image=ctk_img)
+        app.track_cover.image = ctk_img
+    else:
+        app.track_cover.configure(image=None, text="")
+
+def on_get_media_info():
+    threading.Thread(
+        target=load_media_info, 
+        args=(app, update_ui), 
+        daemon=True
+    ).start()
+
+
+def quit_app():
+    print("bye!")
+    app.quit()
+
+# def on_get_media_info():
+#     data = asyncio.run(get_media_info())
+
+#     if not data:
+#         app.title_label.configure(text="Title: No Media Active")
+#         app.artist_label.configure(text="Artist: -")
+#         return
+
+#     app.title_label.configure(text=data["title"])
+#     app.artist_label.configure(text=data["artist"])
+
+#     if data["cover_image"]:
+#         img = data["cover_image"].resize((90, 90))
+#         ctk_img = ctk.CTkImage(light_image=img, dark_image=img, size=(90, 90))
+#         app.track_cover.configure(image=ctk_img)
+#         app.track_cover.image = ctk_img
+#     else:
+#         app.track_cover.configure(image=None, text="")
+
+
+
 if __name__ == "__main__":
     app = Overlay()
-    media_info = MediaInfoWindow(app)
     app.mainloop()
---
+++ media_info.py
@@ -0,0 +1,89 @@
+import asyncio
+from io import BytesIO
+
+from PIL import Image
+
+from datetime import datetime, timezone
+
+from winrt.windows.media.control import GlobalSystemMediaTransportControlsSessionManager
+from winrt.windows.storage.streams import DataReader
+
+def format_time(timespan):
+    total_seconds = int(timespan.total_seconds())
+    minutes = total_seconds // 60
+    seconds = total_seconds % 60
+    return f"{minutes}:{seconds:02}"
+
+async def get_media_info():
+    sessions = await GlobalSystemMediaTransportControlsSessionManager.request_async()
+    session = sessions.get_current_session()
+
+    # if theres no active media session, return None
+    if not session:
+        return None
+
+    # get media properties and timeline properties
+    info = await session.try_get_media_properties_async()
+    timeline = session.get_timeline_properties()
+    actual_position = timeline.position
+    
+    if session.get_playback_info().playback_status == 4: # check if track is playing (4 - playing, 3 - paused, 5 - changing, maybe)
+        # calculate the time that has passed since the last update and add it to the position
+        time_diff = datetime.now(timezone.utc) - timeline.last_updated_time
+        actual_position += time_diff
+    
+    if actual_position > timeline.end_time: # set limit for position so it does not go beyond the track
+        actual_position = timeline.end_time
+
+    # get track cover if available
+    cover_image = None
+    if info.thumbnail:
+        stream = await info.thumbnail.open_read_async()
+        reader = DataReader(stream)
+        await reader.load_async(stream.size)
+
+        data = bytearray(stream.size)
+        reader.read_bytes(data)
+
+        cover_image = Image.open(BytesIO(data)).convert("RGBA")
+
+    return {
+        "title": info.title or "-",
+        "artist": info.artist or "-",
+        "position": format_time(actual_position) or "0:00 -",
+        "duration": format_time(timeline.end_time) or "0:00 -",
+        "freeze position": format_time(timeline.position) or "0:00 -",
+        "last updated": timeline.last_updated_time.isoformat() if timeline.last_updated_time else "-",
+        "playback_status": session.get_playback_info().playback_status,
+        "album": info.album_title or "-",
+        "genres": ", ".join(info.genres) if info.genres else "-",
+        "cover_image": cover_image,
+    }
+
+def load_media_info(app, callback_func):
+    try:
+        # run the async function in the event loop and get the result
+        data = asyncio.run(get_media_info())
+        
+        if data:
+            print(" - - - Current Media - - - ")
+            for key, value in data.items():
+                if key != "cover_image":
+                    print(f"{key}: {value}")
+        
+        app.after(0, callback_func, data)
+    except Exception as e:
+        print(f"Error loading media: {e}")
+        app.after(0, callback_func, None)
+    
+
+def print_media_info():
+    data = asyncio.run(get_media_info())
+
+    if not data:
+        print("No active media session (main)")
+        return
+
+    print(" - - - Current Media - - - ")
+    for key, value in data.items():
+        print(f"{key}: {value}")
